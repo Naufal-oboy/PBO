6475ab35416ed9b3834bc9b99091e068d998068a
Naufal <xorpro08@gmail.com>
2026-04-05 20:54:55 +0800

    POSTTEST_3

---
+++ POSTTEST_3/src/LapanganIndoor.java
@@ -0,0 +1,23 @@
+class LapanganIndoor extends LapanganFutsal {
+    private boolean tersediaAC;
+
+    LapanganIndoor(int id, String nama, String lokasi, int harga, String status,
+                   boolean tersediaAC) {
+        super(id, nama, lokasi, harga, status);
+        this.tersediaAC = tersediaAC;
+    }
+
+    public boolean isTersediaAC() { return tersediaAC; }
+
+    public void setTersediaAC(boolean tersediaAC) {
+        this.tersediaAC = tersediaAC;
+    }
+
+    @Override
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Tipe    : Indoor");
+        System.out.println("AC      : " + (this.tersediaAC ? "Ada" : "Tidak Ada"));
+        System.out.println("--------------------------------------");
+    }
+}
